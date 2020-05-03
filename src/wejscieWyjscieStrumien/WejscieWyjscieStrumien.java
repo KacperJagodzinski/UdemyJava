718304869d0c7afa4ddace83fe470658481094e6
Kacper <kacper.jagodzinski@onet.eu>
2020-05-03 14:52:16 +0200

    Filtrowanie Strumieni

--- src/wejscieWyjscieStrumien/WejscieWyjscieStrumien.java
+++ src/wejscieWyjscieStrumien/WejscieWyjscieStrumien.java
@@ -10,11 +10,21 @@ public class WejscieWyjscieStrumien {
         InputStream inS;
         OutputStream outS;
 
+        File plik = new File("nazaPliku.txt");
+        Writer writer2 = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(plik)));
+
         Reader reader;
-        Writer writer = new FileWriter("nazwaPliku.txt");
-        writer.write("lalalala");
+        //FileWriter pozwala nam zapisac do pliku
+        //Połączenie strumienia z buforem (zapis tymczasowych danych)
+        //Na poczatku sa przetrzymywane w buforze w pamieci i przy zamknieciu sa wysylane wszystkie na raz
+        //Program dziala szybciej
+        Writer writer = new BufferedWriter(new FileWriter("nazwaPliku.txt"));
+        ((BufferedWriter) writer).newLine();
+        ((BufferedWriter) writer).write("lalala linia nastepna");
+        ((BufferedWriter) writer).close();
+        //writer.write("lalalala");
         //zamykamy głowne po to żeby mieć pewność że zmiany się zapisały
-        writer.close();
+        //writer.close();
 
     }
     //przyjmnuje wszystkie podklasy potem mozemy rzutowac typowa klasa abstrakcyjna
