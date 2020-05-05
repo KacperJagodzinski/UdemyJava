bff75cef29a3432979da6ba27963408caddd987c
Kacper <kacper.jagodzinski@onet.eu>
2020-05-05 13:33:05 +0200

    Rysowanie

---
+++ src/rysowanie/Rysowanie.java
@@ -0,0 +1,55 @@
+package rysowanie;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Rysowanie extends JFrame {
+    public Rysowanie() {
+        this.setTitle("Rysowanie");
+        this.setBounds(250, 300, 300, 250);
+
+        this.getContentPane().add(panelDoRysowania);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new Rysowanie().setVisible(true);
+    }
+
+    private PanelRysowiak panelDoRysowania = new PanelRysowiak();
+}
+
+class PanelRysowiak extends JPanel {
+    public PanelRysowiak() {
+        super();
+
+        this.add(new JButton("Test") {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+            }
+
+        });
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawString("Napis", 0, 40);
+
+        g.drawImage(new ImageIcon("kropelka.gif").getImage(), 40, 80, null);
+
+        g.drawRect(40, 40, 120, 80);
+
+        g.drawLine(60, 60, 80, 20);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        System.out.println(i++);
+    }
+
+    public static int i = 0;
+}
